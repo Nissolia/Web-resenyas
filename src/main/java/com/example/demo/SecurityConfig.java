7e6171772bbc8ed9480de7ea8b249b0607b67fc3
Noelia\Portatil_Noelia <nissoliadesing@gmail.com>
2025-02-21 18:38:31 +0100

    Configurando el SecurityConfig

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    UserDetailsService userDetailsService() {
+		var userDetailsService = new InMemoryUserDetailsManager();
+		UserDetails user1 = User.withUsername("admin").password(passwordEncoder().encode("admin")).authorities("write")
+				.build();
+		userDetailsService.createUser(user1);
+		return userDetailsService;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
